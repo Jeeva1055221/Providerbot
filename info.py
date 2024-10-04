6cc10409d621304a041460205e51d3fdab65ccc8
Jeeva1055221 <151960878+Jeeva1055221@users.noreply.github.com>
2024-10-04 23:30:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -50,8 +50,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False)) # True if you want no results messages in Log Channel
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://kutty:1512@cluster1.tb1y11c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Ajifilterbot")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://jeevanantham8157:1055221@filterbot.43l7b.mongodb.net/?retryWrites=true&w=majority&appName=Filterbot")
+DATABASE_NAME = environ.get('DATABASE_NAME', "filterbot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Verify
